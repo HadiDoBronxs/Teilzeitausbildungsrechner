7e704483aebfcfec04e7a749298e1e4e8ab896b6
ebischof <ebischof@stud.hs-heilbronn.de>
2025-10-24 19:07:02 +0200

    added vitest.setup.js file

---
+++ vitest.setup.js
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/vitest";
+
+vi.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    t: (key, opts) =>
+      key === "fulltimeHours.error"
+        ? `ERR ${opts?.min}-${opts?.max}`
+        : key,
+  }),
+}));
