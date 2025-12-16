0ed8bc2edd5a47b1326d9f58efe2166a73bdce54
ebischof <ebischof@stud.hs-heilbronn.de>
2025-12-16 23:37:26 +0100

    60: uses the browser's beforeunload event to show a warning dialog when leaving/reloading page

--- src/routes/CompactView.jsx
+++ src/routes/CompactView.jsx
@@ -13,6 +13,7 @@ import ResultBottomBar from "../components/ResultBottomBar.jsx";
 import PDFViewer from "../components/PDFViewer.jsx";
 import Button from "../components/ui/Button.jsx";
 import { useCalculator } from "../features/calcDuration/useCalculator.js";
+import { useUnsavedChangesWarning } from "../features/calcDuration/useUnsavedChangesWarning.js";
 
 const MAIN_ID = "main";
 const MAIN_HEADING_ID = "main-heading";
@@ -29,6 +30,7 @@ export default function CompactView() {
     t,
     schoolDegreeId,
     fulltimeHours,
+    parttimeHours,
     handleSchoolDegreeSelect,
     handleFulltimeHoursChange,
     handleParttimeHoursChange,
@@ -45,6 +47,15 @@ export default function CompactView() {
     resetCount,
     handleReset,
   } = useCalculator();
+
+  // Set up browser warning when user tries to close tab with unsaved changes
+  useUnsavedChangesWarning({
+    fulltimeHours,
+    parttimeHours,
+    fullDurationMonths: formValues.fullDurationMonths,
+    schoolDegreeId,
+    qualificationSelection,
+  });
 
   return (
     <>
